11150bfb16747a8034d803beb6988c929c3b6bf7
Susana <sarazasusan2@gmail.com>
2025-02-25 11:50:49 -0500

    terminamos tuplas

--- App/tuplas.py
+++ App/tuplas.py
@@ -1,6 +1,13 @@
 #tuplas no se pueden modificar
 from lib2to3.fixes.fix_tuple_params import tuple_name
 
-t: tuple[int, int, int] = (2, 3, 4)
+t1: tuple[int, int, int] = (2, 3, 4)
 t2 : tuple[int]  = (2,) #se grega la coma para identificar que es una tupla
 
+x, y, z = t1 #Empaquetar las variables en la tupla
+
+print(x)
+print(y)
+print(z)
+
+x, y = y, x # Intercarmbiar los valores
